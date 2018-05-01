3324fd3c6a0799d1ff7f43cf8a6cc011a5fb2f2f
Alvin Shanmugam <alvins2@illinois.edu>
2018-05-01 15:56:02 -0500

    Final

--- app/src/main/java/edu/illinois/cs/cs125/faceceleb/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/faceceleb/MainActivity.java
@@ -3,6 +3,11 @@ package edu.illinois.cs.cs125.faceceleb;
 import com.microsoft.projectoxford.face.*;
 import com.microsoft.projectoxford.face.contract.*;
 import java.io.*;
+
+import android.app.*;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.*;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -14,6 +19,8 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.net.Uri;
+
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -27,13 +34,12 @@ public class MainActivity extends AppCompatActivity {
     /** Constant to request an image capture. */
     private static final int IMAGE_CAPTURE_REQUEST_CODE = 1;
 
-//    /** Constant to perform a read file request. */
-//    private static final int READ_REQUEST_CODE = 42;
+    /** Constant to perform a read file request. */
+    private static final int READ_REQUEST_CODE = 42;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTitle("How Old Do You Look?");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -46,14 +52,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        final ImageButton openFile = findViewById(R.id.openFile);
-//        openFile.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(final View v) {
-//                Log.d(TAG, "Open file button clicked");
-//                startOpenFile();
-//            }
-//        });
+        detectionProgressDialog = new ProgressDialog(this);
+
+
+        final ImageButton openFile = findViewById(R.id.openFile);
+        openFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Open file button clicked");
+                startOpenFile();
+            }
+        });
 
     }
 
@@ -65,15 +74,15 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(takePictureIntent, IMAGE_CAPTURE_REQUEST_CODE);
     }
 
-//    /**
-//     * Start an open file dialog to look for image files.
-//     */
-//    private void startOpenFile() {
-//        Intent openFileIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-//        openFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
-//        openFileIntent.setType("image/*");
-//        startActivityForResult(openFileIntent, READ_REQUEST_CODE);
-//    }
+    /**
+     * Start an open file dialog to look for image files.
+     */
+    private void startOpenFile() {
+        Intent openFileIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        openFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        openFileIntent.setType("image/*");
+        startActivityForResult(openFileIntent, READ_REQUEST_CODE);
+    }
 
     @Override
     public void onActivityResult(final int requestCode, final int resultCode,
@@ -83,108 +92,113 @@ public class MainActivity extends AppCompatActivity {
             if (requestCode == IMAGE_CAPTURE_REQUEST_CODE) {
                 Bundle extras = resultData.getExtras();
                 Bitmap userPhoto = (Bitmap) extras.get("data");
-                photoView = findViewById(R.id.imageView);
+                photoView = findViewById(R.id.photoView);
                 if (userPhoto != null) {
                     photoView.setImageBitmap(userPhoto);
-                    Face[] result = findFace(userPhoto);
-                    try {
-                        Toast.makeText(getApplicationContext(), "You Look " + result[0].toString() + " Years Old", Toast.LENGTH_LONG).show();
-                    } catch (Exception e){
-                        Toast.makeText(getApplicationContext(), "No Face Detected", Toast.LENGTH_LONG).show();
-                    }
+                    detectAndFrame(userPhoto);
                 } else {
                     Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
                 }
-
-                /** Most of it is done. After taking a picture with the camera, the photo will be stored in the "Bitmap photo".
-                 *
-                 *  What's left to do is:
-                 *
-                 *  - To give the Bitmap the the API (maybe convert it into a jpeg if needed)
-                 *  - Tell the API to do wtv it needs, and get it to return a the photo of the celebrity
-                 *  - Convert the photo into either
-                 *      - a Uri, and set the Uri to photoURi in CODE A
-                 *    or
-                 *      - a Bitmap, and set the Bitmap to filePhoto in CODE B
-                 */
-
-                /*String[] photos = null;
-                AssetManager mngr = getAssets();
-                try {
-                    photos = mngr.list("faces");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    for (int i = 0; i < photos.length; i++) {
-                            InputStream currPhoto = mngr.open("faces" + "/" + photos[i] + ".jpg");
-                            Bitmap currPhotoJPG = BitmapFactory.decodeStream(currPhoto);
-                            ByteArrayOutputStream currStream = new ByteArrayOutputStream();
-                            currPhotoJPG.compress(Bitmap.CompressFormat.JPEG, 100, currStream);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();*/
-                }
-                /**    CODE A - only remove this and others
-
-                 Uri photoUri = _____________________
-                 try {
-                 filePhoto = MediaStore.Images.Media.getBitmap(
-                 this.getContentResolver(), photoUri);
-                 } catch (IOException e) {
-                 return;
-                 }
-
-                 /**   CODE B - only remove this and others
-
-                 filePhoto = ___________________
-
-                 */     // only remove this and others
             }
 
-//            else if (requestCode == READ_REQUEST_CODE) {
-//                Bitmap filePhoto;
-//                Uri photoUri = resultData.getData();
-//                try {
-//                    filePhoto = MediaStore.Images.Media.getBitmap(
-//                            this.getContentResolver(), photoUri);
-//                } catch (IOException e) {
-//                    return;
-//                }
-//
-//                photoView = findViewById(R.id.photoView);
-//                photoView.setImageBitmap(filePhoto);
-//            }
-        }
+            else if (requestCode == READ_REQUEST_CODE) {
+                Bitmap filePhoto;
+                Uri photoUri = resultData.getData();
+                try {
+                    filePhoto = MediaStore.Images.Media.getBitmap(
+                            this.getContentResolver(), photoUri);
+                } catch (IOException e) {
+                    return;
+                }
 
-    private Face[] findFace(final Bitmap bitmap) {
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-            ByteArrayInputStream inputStream =
-                    new ByteArrayInputStream(outputStream.toByteArray());
-            AsyncTask<InputStream, String, Face[]> detectTask =
-                    new AsyncTask<InputStream, String, Face[]>() {
-                        @Override
-                        protected Face[] doInBackground(InputStream... params) {
-                            try {
-                                Face[] result = faceServiceClient.detect(
-                                        params[0],
-                                        false,
-                                        false,
-                                        new FaceServiceClient.FaceAttributeType[]{FaceServiceClient.FaceAttributeType.Age}
-                                );
-                                return result;
-                            } catch (Exception e) {
-                                return new Face[1];
-                            }
-                        }
-                    };
-            detectTask.execute(inputStream);
-            return null;
-        } catch (Exception e) {
-            return new Face[1];
+                photoView = findViewById(R.id.photoView);
+                photoView.setImageBitmap(filePhoto);
+                detectAndFrame(filePhoto);
             }
         }
     }
+
+    private ProgressDialog detectionProgressDialog;
+
+    private void detectAndFrame(final Bitmap imageBitmap)
+    {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        ByteArrayInputStream inputStream =
+                new ByteArrayInputStream(outputStream.toByteArray());
+        AsyncTask<InputStream, String, Face[]> detectTask =
+                new AsyncTask<InputStream, String, Face[]>() {
+                    @Override
+                    protected Face[] doInBackground(InputStream... params) {
+                        try {
+                            publishProgress("Detecting...");
+                            Face[] result = faceServiceClient.detect(
+                                    params[0],
+                                    true,         // returnFaceId
+                                    true,        // returnFaceLandmarks
+                                    new FaceServiceClient.FaceAttributeType[]{FaceServiceClient.FaceAttributeType.Age}           // returnFaceAttributes: a string like "age, gender"
+                            );
+                            if (result == null)
+                            {
+                                publishProgress("Detection Finished. Nothing detected");
+                                return null;
+                            }
+                            publishProgress(
+                                    String.format("Detection Finished. %d face(s) detected",
+                                            result.length));
+                            return result;
+                        } catch (Exception e) {
+                            publishProgress("Detection failed");
+                            return null;
+                        }
+                    }
+                    @Override
+                    protected void onPreExecute() {
+                        detectionProgressDialog.show();
+                    }
+                    @Override
+                    protected void onProgressUpdate(String... progress) {
+                        detectionProgressDialog.setMessage(progress[0]);
+                    }
+                    @Override
+                    protected void onPostExecute(Face[] result) {
+                        detectionProgressDialog.dismiss();
+                        if (result == null) {
+                            return;
+                        }
+                        ImageView imageView = (ImageView)findViewById(R.id.photoView);
+                        imageView.setImageBitmap(drawFaceRectanglesOnBitmap(imageBitmap, result));
+                        try {
+                            Toast.makeText(getApplicationContext(), "You Look " + result[0].faceAttributes.age + " Years Old", Toast.LENGTH_LONG).show();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "No Face Detected", Toast.LENGTH_LONG).show();
+                        }
+                        imageBitmap.recycle();
+                    }
+                };
+        detectTask.execute(inputStream);
+    }
+
+    private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
+        Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.RED);
+        int stokeWidth = 2;
+        paint.setStrokeWidth(stokeWidth);
+        if (faces != null) {
+            for (Face face : faces) {
+                FaceRectangle faceRectangle = face.faceRectangle;
+                canvas.drawRect(
+                        faceRectangle.left,
+                        faceRectangle.top,
+                        faceRectangle.left + faceRectangle.width,
+                        faceRectangle.top + faceRectangle.height,
+                        paint);
+            }
+        }
+        return bitmap;
+    }
+}
